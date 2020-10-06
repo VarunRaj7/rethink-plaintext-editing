df911ecbdde054825fc5c2259d6d94be4dc94b3c
VarunRaj7 <rvarunraj1@gmail.com>
2020-10-06 03:24:08 -0400

    adding html parser to markdown

--- components/MarkdownEditor/index.js
+++ components/MarkdownEditor/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MdEditor from 'react-markdown-editor-lite';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from 'react-markdown/with-html';
 import css from './style.module.css';
 
 function MarkdownEditor({ value, handleValue }) {
@@ -14,6 +14,7 @@ function MarkdownEditor({ value, handleValue }) {
         value={value}
         onChange={handleChange}
         renderHTML={text => <ReactMarkdown source={text} />}
+        escapeHtml={false}
       />
     </div>
   );
--- components/MarkdownPreviewer/index.js
+++ components/MarkdownPreviewer/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from 'react-markdown/with-html';
 
 import css from './style.module.css';
 
 function MarkdownPreviewer({ value }) {
   return (
     <div>
-      <ReactMarkdown source={value} className={css.content} />
+      <ReactMarkdown
+        source={value}
+        className={css.content}
+        escapeHtml={false}
+      />
     </div>
   );
 }
